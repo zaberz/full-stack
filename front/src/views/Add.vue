ecd28648cf15b1164b8c66ac2bed7d90301f8900
zaber <274588107@qq.com>
2019-09-22 23:02:54 +0800

    编写前端todolist代码

---
+++ front/src/views/Add.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="">
+    <input type="text" placeholder="请输入任务内容" v-model="task">
+    <button @click="add">添加</button>
+  </div>
+</template>
+
+<script>
+import taskStore from '@/libs/taskStore';
+
+export default {
+  data: ()=> ({
+    task: ''
+  }),
+  methods: {
+    async add() {
+      console.log(taskStore);
+      await taskStore.setItem(+new Date(), this.task);
+      this.$router.push({
+        name: 'home'
+      })
+    }
+  }
+}
+</script>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <task-item v-for="(task,index) in list" :task="task.content" :time="task.time" :key="index" @change="removeTask(task)"></task-item>
+    <router-link to="add">添加</router-link>
+  </div>
+</template>
+
+<script>
+import TaskItem from '@/components/TaskItem';
+import taskStore from '@/libs/taskStore';
+export default {
+  name: 'home',
+  components: {
+    TaskItem,
+  },
+  data: ()=> {
+    let list = []
+    taskStore.iterate((value, key)=> {
+      list.push({
+        key: key,
+        time: new Date(parseInt(key)).toLocaleString(),
+        content: value,
+        status: 1
+      })
+    })
+    return {
+      list,
+    }
+  },
+  mounted() {
+  },
+  methods: {
+    removeTask(task) {
+      let index = this.list.findIndex((item) => (item === task))
+      let key = task.key
+      taskStore.removeItem(key).then(()=> {
+        this.list.splice(index, 1)
+      })
+    }
+  }
+}
+</script>
